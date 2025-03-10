226584c1143368c9c4272e94fb1557f43d53e16e
RevanAbdullayevv <Hp@DESKTOP-ANVKM7S>
2025-03-11 02:20:08 +0400

    Add project files.

---
+++ CodeCalisma1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/*1-Den 100-e qeder ededler arasinda cut ededleri tapan alqoritm yazin.(FOR LOOP)
+    /*for(int a=1; a<=100; a++)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+}*/
+
+/*(WHILE LOOP))
+/*int a = 1;
+while (a <= 100)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+      
+    }
+    a++;
+}*/
+
+/*Array daxilinde yalniz sade ededleri capa veren alqoritm yazin(FOR LOOP)
+/*int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+Console.WriteLine("Sade Ededler:");
+Console.WriteLine(2);
+Console.WriteLine(3);
+Console.WriteLine(5);
+Console.WriteLine(7);
+
+for (int i = 2; i <= 40; i++)
+    
+{
+    if (i % 2 != 0 && i%3!=0 && i%5!=0 && i%7!=0)
+    {
+        Console.WriteLine(i);
+
+    }
+
+}*/
+
+/*(WHILE LOOP)
+/*int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+Console.WriteLine("Sade Ededdir");
+Console.WriteLine(2);
+Console.WriteLine(3);
+Console.WriteLine(5);
+Console.WriteLine(7);
+int i = 2;
+while (i <= 40)
+{
+    if(i%2!=0 && i%3!=0 && i%5!=0 && i % 7 != 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}*/
+
